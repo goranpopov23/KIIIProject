273a10bb97dc37cfdc73cba1060ff4aa35046b61
Goran Popov <goranpopov322@gmail.com>
2024-09-24 08:27:53 +0200

    Sucessfully deployed application to K8s!

--- KIIIDjango/settings.py
+++ KIIIDjango/settings.py
@@ -78,11 +78,11 @@ WSGI_APPLICATION = 'KIIIDjango.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'cars2',
-        'USER': 'goran',
-        'PASSWORD': 'goran',
-        'HOST': 'db',
-        'PORT': '5432'
+        'NAME': os.environ.get('POSTGRES_DB'),
+        'USER': os.environ.get('POSTGRES_USER'),
+        'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
+        'HOST': os.environ.get('POSTGRES_HOST'),
+        'PORT': os.environ.get('POSTGRES_PORT')
     }
 }
 
